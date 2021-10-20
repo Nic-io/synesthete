f504ed16e07131973be35f8d4e1fe166a59924be
Nic-io <nicolas@ecliptic.fr>
2021-10-20 17:02:37 +0200

    Structurting project, added Threads, preparing ADC for touchScreen

---
+++ src/gui/gui.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2021 Nicolas Blazevic <nicolas@ecliptic.fr>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef __SYNESTHETE_GUI_H
+#define __SYNESTHETE_GUI_H
+
+#include <logging/log.h>
+#include <zephyr.h>
+#include <device.h>
+#include <drivers/display.h>
+#include <console/console.h>
+#include <sys/printk.h>
+
+#define DISPLAY_DEV_NAME DT_LABEL(DT_INST(0, ilitek_ili9340))
+
+int gui_init();
+void gui_draw_wave();
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,217 +1,67 @@
 /*
- * Copyright (c) 2019 Jan Van Winkel <jan.van_winkel@dxplore.eu>
- *
- * Based on ST7789V sample:
- * Copyright (c) 2019 Marc Reilly
+ * Copyright (c) 2021 Nicolas Blazevic <nicolas@ecliptic.fr>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <logging/log.h>
-LOG_MODULE_REGISTER(sample, LOG_LEVEL_INF);
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 #include <zephyr.h>
 #include <device.h>
 #include <drivers/display.h>
 #include <console/console.h>
 #include <sys/printk.h>
+#include "gui.h"
 
 
-#if DT_NODE_HAS_STATUS(DT_INST(0, ilitek_ili9340), okay)
-#define DISPLAY_DEV_NAME DT_LABEL(DT_INST(0, ilitek_ili9340))
-#endif
+int32_t tune_table[320] = { 0xFFFFFFFF };
+int valueIn;
 
-#if DT_NODE_HAS_STATUS(DT_INST(0, solomon_ssd1306fb), okay)
-#define DISPLAY_DEV_NAME DT_LABEL(DT_INST(0, solomon_ssd1306fb))
-#endif
-
-#if DT_NODE_HAS_STATUS(DT_INST(0, solomon_ssd16xxfb), okay)
-#define DISPLAY_DEV_NAME DT_LABEL(DT_INST(0, solomon_ssd16xxfb))
-#endif
-
-#if DT_NODE_HAS_STATUS(DT_INST(0, sitronix_st7789v), okay)
-#define DISPLAY_DEV_NAME DT_LABEL(DT_INST(0, sitronix_st7789v))
-#endif
-
-#if DT_NODE_HAS_STATUS(DT_INST(0, sitronix_st7735r), okay)
-#define DISPLAY_DEV_NAME DT_LABEL(DT_INST(0, sitronix_st7735r))
-#endif
-
-#if DT_NODE_HAS_STATUS(DT_INST(0, fsl_imx6sx_lcdif), okay)
-#define DISPLAY_DEV_NAME DT_LABEL(DT_INST(0, fsl_imx6sx_lcdif))
-#endif
-
-#ifdef CONFIG_SDL_DISPLAY_DEV_NAME
-#define DISPLAY_DEV_NAME CONFIG_SDL_DISPLAY_DEV_NAME
-#endif
-
-#ifdef CONFIG_DUMMY_DISPLAY_DEV_NAME
-#define DISPLAY_DEV_NAME CONFIG_DUMMY_DISPLAY_DEV_NAME
-#endif
-#ifdef CONFIG_ARCH_POSIX
-#include "posix_board_if.h"
-#endif
-
-#ifdef CONFIG_ARCH_POSIX
-#define RETURN_FROM_MAIN(exit_code) posix_exit_main(exit_code)
-#else
-#define RETURN_FROM_MAIN(exit_code) return
-#endif
-
-enum corner {
-	TOP_LEFT,
-	TOP_RIGHT,
-	BOTTOM_RIGHT,
-	BOTTOM_LEFT
-};
-
-typedef void (*fill_buffer)(enum corner corner, uint8_t grey, uint8_t *buf,
-			    size_t buf_size);
-
-
-#ifdef CONFIG_ARCH_POSIX
-static void posix_exit_main(int exit_code)
-{
-#if CONFIG_TEST
-	if (exit_code == 0) {
-		LOG_INF("PROJECT EXECUTION SUCCESSFUL");
-	} else {
-		LOG_INF("PROJECT EXECUTION FAILED");
-	}
-#endif
-	posix_exit(exit_code);
-}
-#endif
-
-static void fill_buffer_rgb888(enum corner corner, uint8_t grey, uint8_t *buf,
-			       size_t buf_size)
-{
-	uint32_t color = 0;
-
-	switch (corner) {
-	case TOP_LEFT:
-		color = 0x00FF0000u;
-		break;
-	case TOP_RIGHT:
-		color = 0x0000FF00u;
-		break;
-	case BOTTOM_RIGHT:
-		color = 0x000000FFu;
-		break;
-	case BOTTOM_LEFT:
-		color = grey << 16 | grey << 8 | grey;
-		break;
-	}
-
-	for (size_t idx = 0; idx < buf_size; idx += 3) {
-		*(buf + idx + 0) = color >> 16;
-		*(buf + idx + 1) = color >> 8;
-		*(buf + idx + 2) = color >> 0;
+void gui_thread(void *p1, void *p2, void *p3){
+	while (1){
+		gui_draw_wave(tune_table);
+		k_sleep(K_MSEC(40));
 	}
 }
+
+void update_table(void *p1, void *p2, void *p3){
+	while (1){
+		valueIn = console_getchar();
+		printk("Acquired %d \n", valueIn);
+		memset(tune_table, valueIn*40, 320*4);
+	}
+}
+
+#define STACK_SIZE 1024
+#define GUI_PRIO 3
+#define TABLE_PRIO 1
+
+K_THREAD_STACK_DEFINE(gui_stack_area, STACK_SIZE);
+K_THREAD_STACK_DEFINE(table_stack_area, STACK_SIZE);
+
+struct k_thread guiThread;
+struct k_thread tableThread;
 
 void main(void)
 {
-	size_t x;
-	size_t y;
-	size_t rect_w;
-	size_t rect_h;
-	size_t h_step;
-	size_t scale;
-	size_t grey_count;
-	uint8_t *buf;
-	int32_t grey_scale_sleep;
-	const struct device *display_dev;
-	struct display_capabilities capabilities;
-	struct display_buffer_descriptor buf_desc;
-	size_t buf_size = 0;
-	fill_buffer fill_buffer_fnc = NULL;
-
 	console_init();
+	gui_init();
 
-	LOG_INF("Display sample for %s", DISPLAY_DEV_NAME);
+	k_tid_t my_tid = k_thread_create(&guiThread, gui_stack_area,
+                                 K_THREAD_STACK_SIZEOF(gui_stack_area),
+                                 gui_thread,
+                                 NULL, NULL, NULL,
+                                 GUI_PRIO, 0, K_NO_WAIT);
 
-	display_dev = device_get_binding(DISPLAY_DEV_NAME);
-
-	if (display_dev == NULL) {
-		LOG_ERR("Device %s not found. Aborting sample.",
-			DISPLAY_DEV_NAME);
-		RETURN_FROM_MAIN(1);
-	}
-
-	display_get_capabilities(display_dev, &capabilities);
-
-	int bufthickness = 1;
-	buf_size = capabilities.x_resolution*bufthickness*3;
-
-	
-	buf = k_malloc(buf_size);
-	
-	if (buf == NULL) {
-		LOG_ERR("Could not allocate memory. Aborting sample.");
-		RETURN_FROM_MAIN(1);
-	}
-
-	display_blanking_off(display_dev);
-
-	buf_desc.buf_size = buf_size;
-	buf_desc.pitch = capabilities.x_resolution;
-	buf_desc.width = capabilities.x_resolution;
-	buf_desc.height = bufthickness;
-
-	void * memestart = 0x20000000;
-	void * memptr = memestart;
-	(void)memset(buf, 0xFF, buf_size);
-
-	for(int k=0; k<240; k++){
-		display_write(display_dev, 0, k, &buf_desc, buf);
+	k_tid_t my_tid2 = k_thread_create(&tableThread, table_stack_area,
+                                 K_THREAD_STACK_SIZEOF(table_stack_area),
+                                 update_table,
+                                 NULL, NULL, NULL,
+                                 TABLE_PRIO, 0, K_NO_WAIT);
+	while (1)
+	{
+		k_msleep(100);
 	}
 	
-	while (1) {		
-
-		for(int k=0; k<240; k+=3){
-			
-			for (size_t idx = 0; idx < buf_size; idx += 9) {
-
-			  /**(buf + idx + 0) = *(uint32_t*)(memptr + meminc);
-				*(buf + idx + 1) = *(uint32_t*)(memptr + meminc);
-				*(buf + idx + 2) = *(uint32_t*)(memptr + meminc);
- 
-				*(buf + idx + 3) = *(uint32_t*)(memptr + meminc) >> 8;
-				*(buf + idx + 4) = *(uint32_t*)(memptr + meminc) >> 8;
-				*(buf + idx + 5) = *(uint32_t*)(memptr + meminc) >> 8;
-				 
-				*(buf + idx + 6) = *(uint32_t*)(memptr + meminc) >> 16;
-				*(buf + idx + 7) = *(uint32_t*)(memptr + meminc) >> 16;
-				*(buf + idx + 8) = *(uint32_t*)(memptr + meminc) >> 16; */
-
-				*(buf + idx + 0) = *(uint32_t*)(memptr);
-				*(buf + idx + 1) = *(uint32_t*)(memptr) >> 8;
-				*(buf + idx + 2) = *(uint32_t*)(memptr) >> 16;
-
-				*(buf + idx + 3) = *(uint32_t*)(memptr);
-				*(buf + idx + 4) = *(uint32_t*)(memptr) >> 8;
-				*(buf + idx + 5) = *(uint32_t*)(memptr) >> 16;
-				
-				*(buf + idx + 6) = *(uint32_t*)(memptr);
-				*(buf + idx + 7) = *(uint32_t*)(memptr) >> 8;
-				*(buf + idx + 8) = *(uint32_t*)(memptr) >> 16;
-				memptr++;
-			}
-
-			for(int j=0; j<3; j++){
-				display_write(display_dev, 0, k+j, &buf_desc, buf);
-			}
-		}
-		printk("showing memory from %p to %p \n", memestart, memptr);
-		memptr = memestart;
-
-#if CONFIG_TEST
-		if (grey_count >= 1024) {
-			break;
-		}
-#endif
-	}
-
-	RETURN_FROM_MAIN(0);
 }
